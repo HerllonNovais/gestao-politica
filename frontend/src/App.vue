5675a6ee6674f53c048bf1d6b0892c4c20764f40
HerllonNovais <hnnascimento@gmail.com>
2025-05-02 20:27:12 -0300

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <NavBar v-if="isAuthenticated" />
+    <SideMenu v-if="isAuthenticated" />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useAuthStore } from '@/store/auth'
+import NavBar from '@/components/NavBar.vue'
+import SideMenu from '@/components/SideMenu.vue'
+
+const authStore = useAuthStore()
+const isAuthenticated = computed(() => authStore.isAuthenticated)
+</script>
+
+<style scoped>
+.v-main {
+  padding: 20px;
+}
+</style>
