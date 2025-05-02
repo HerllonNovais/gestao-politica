a6af26229255223c62375654bf9a47582eee067a
HerllonNovais <hnnascimento@gmail.com>
2025-05-02 20:31:31 -0300

    Create NavBar.vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar color="primary" dark>
+    <v-app-bar-title>Gestão Política</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn @click="logout">Sair</v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/store/auth'
+
+const authStore = useAuthStore()
+
+function logout() {
+  authStore.handleLogout()
+}
+</script>
