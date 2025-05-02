8a75d60f9cfbc56db47e5df1c150d8f295d69376
HerllonNovais <hnnascimento@gmail.com>
2025-05-02 20:30:56 -0300

    Create Dashboard.vue

---
+++ frontend/src/pages/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1>Dashboard</h1>
+    <iframe
+      :src="metabaseUrl"
+      width="100%"
+      height="800"
+      frameborder="0"
+    ></iframe>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const metabaseUrl = ref('http://metabase:3000/public/dashboard/YOUR_DASHBOARD_ID')
+</script>
